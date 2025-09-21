41ab42d6da4f8055fbf518cae21ffd5bebca1c77
bayyubenjamin <bayyujack@gmail.com>
2025-09-21 19:24:22 +0700

    fix: re

--- wagmi.ts
+++ wagmi.ts
@@ -1,12 +1,12 @@
 // wagmi.ts
 import { http, createConfig } from 'wagmi';
 import { baseSepolia } from 'wagmi/chains';
-import { farcaster } from '@farcaster/miniapp-wagmi-connector';
+import farcaster from '@farcaster/miniapp-wagmi-connector'; // <-- PERBAIKAN: Hapus kurung kurawal {}
 
 export const config = createConfig({
   chains: [baseSepolia],
   connectors: [
-    farcaster(), // <-- Tambahkan konektor Farcaster di sini
+    farcaster(), 
   ],
   transports: {
     [baseSepolia.id]: http(),
