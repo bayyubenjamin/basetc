c2a9bddc1baa63a946ae36dfc1cc12f91b94a499
bayyubenjamin <bayyujack@gmail.com>
2025-09-28 00:21:31 +0700

    pesan commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.alias['@react-native-async-storage/async-storage'] = false;
     return config;
