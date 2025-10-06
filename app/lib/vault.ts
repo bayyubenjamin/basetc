1fa27877aad4797f3b22cf047fe4ef66ece56850
bayyubenjamin <bayyujack@gmail.com>
2025-10-07 06:28:28 +0700

    basetc

--- app/lib/vault.ts
+++ app/lib/vault.ts
@@ -1,19 +1,27 @@
 // app/lib/vault.ts
-export const VAULT_ADDRESS = "0xb962EB2C83982D78878d02fF4226718338877b91" as const;
+export const VAULT_ADDRESS = "0xb962EB2C83982D78878d02fF4226718338877b91" as const; // ganti ke alamat vault kamu
 
 export const vaultABI = [
   {
-    "type":"function","stateMutability":"view","name":"claimed",
-    "inputs":[{"name":"snapshotId","type":"uint256"},{"name":"user","type":"address"}],
-    "outputs":[{"type":"bool"}]
+    "type": "function",
+    "stateMutability": "view",
+    "name": "claimed",
+    "inputs": [
+      { "name": "snapshotId", "type": "uint256" },
+      { "name": "user", "type": "address" }
+    ],
+    "outputs": [{ "type": "bool" }]
   },
   {
-    "type":"function","stateMutability":"nonpayable","name":"claim",
-    "inputs":[
-      {"name":"snapshotId","type":"uint256"},
-      {"name":"amount","type":"uint256"},
-      {"name":"proof","type":"bytes32[]"}
+    "type": "function",
+    "stateMutability": "nonpayable",
+    "name": "claim",
+    "inputs": [
+      { "name": "snapshotId", "type": "uint256" },
+      { "name": "amount", "type": "uint256" },
+      { "name": "proof", "type": "bytes32[]" }
     ],
-    "outputs":[]
+    "outputs": []
   }
 ] as const;
+
