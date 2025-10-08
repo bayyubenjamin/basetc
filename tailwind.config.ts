3d903cad5a0c7f99246ffdea18d7c8a3189b0800
SMOKAHONTAS <69312778+bayyubenjamin@users.noreply.github.com>
2025-10-08 23:30:09 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,17 @@ export default {
     './pages/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        marquee: 'marquee 22s linear infinite',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
