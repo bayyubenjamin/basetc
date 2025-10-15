4db70875c1d8462b4d35e6558b9eee835de78d65
SMOKAHONTAS <69312778+bayyubenjamin@users.noreply.github.com>
2025-10-16 05:48:13 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,11 +46,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="bg-zinc-950">
-<body className="min-h-dvh text-zinc-200 antialiased pt-[calc(env(safe-area-inset-top)+36px)]">
-        {/* ticker global muncul di semua halaman */}
+    <html lang="en">
+      <body className="min-h-dvh antialiased pt-[calc(env(safe-area-inset-top)+36px)] bg-[var(--bg)] text-[var(--text)]">
         <Ticker />
-        {/* HANYA pemicu otomatis tanpa UI */}
         <SilentAddMiniApp />
         {children}
       </body>
