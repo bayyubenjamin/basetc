da47f885d924570e5e5a112224c97348fdab4018
SMOKAHONTAS <69312778+bayyubenjamin@users.noreply.github.com>
2025-10-17 05:51:22 +0700

    Update Monitoring.tsx

--- app/components/Monitoring.tsx
+++ app/components/Monitoring.tsx
@@ -788,7 +788,7 @@ const Monitoring: FC = () => {
                 className="text-[13px] font-bold"
                 style={{ color: "#000" }}
               >
-                Mint progress:{" "}
+                Progress:{" "}
                 {miningPct >= 100
                   ? "FULL — Please claim your $BaseTC"
                   : `${miningPct.toFixed(1)}%`}
