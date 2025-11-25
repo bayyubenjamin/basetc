d00aa3a3115957fadfe140d6737e35e8b10ac88f
SMOKAHONTAS <69312778+bayyubenjamin@users.noreply.github.com>
2025-11-25 17:54:51 +0700

    Update Market.tsx

--- app/components/Market.tsx
+++ app/components/Market.tsx
@@ -410,7 +410,7 @@ const Market: FC = () => {
      Helper kecil: label diskon per tier (UI only)
      ============================= */
   const discountLabel = (id: TierID) =>
-    id === "basic" ? " " : id === "pro" ? "DISCOUNT 65%" : "DISCOUNT 50%";
+    id === "basic" ? " " : id === "pro" ? "DISCOUNT 30%" : "DISCOUNT 50%";
 
   /* =============================
      UI - Neumorphism + kontras teks
